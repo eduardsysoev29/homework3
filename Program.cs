571be5fe1fee67d64e2e57c53726de0f3bc0fb5c
Eduard Sysoev <krikisosedey@bk.ru>
2023-08-11 20:10:34 +0300

    add changes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberS = Math.Abs(number).ToString();
+
+Console.WriteLine(numberS.Length > 2 ? $"{number} -> {numberS[2]}" : $"{numberS} -> Отсутствует третья цифра");
+
+/*
+if(numberS.Length > 2) {
+    Console.WriteLine($"{number} -> {numberS[2]}");
+} else {
+    Console.WriteLine($"{number} -> Отсутствует третья цифра ");
+}
+*/
